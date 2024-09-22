612c996a28ba9eff9327f051261d24dc2d0ea5a2
Nelli Gorlova <nelli7035@gmail.com>
2024-09-23 02:08:40 +0600

    added random selection of index from array

--- src/components/playingField/playingField.tsx
+++ src/components/playingField/playingField.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
 import './playingField.css';
+import {useState} from "react";
 
 
 const getField = () => {
     const field = [];
+
     for (let i: number = 0; i < 36; i++) {
         field.push({
             hasItem: false,
             clicked: false,
         });
     }
+
+    const randomIndex = Math.floor(Math.random() * 36);
+    field[randomIndex].hasItem = true;
+
     return field;
+
 }
 
-const PlayingField = () => {
+const PlayingField: React.FC = () => {
 
+    const [items, setItems] = useState(getField());
 
     return (
         <div className="playing-field">
